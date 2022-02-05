50395d6d4cca5dc394f5d6905416de5c987dbc2a
Diego Escobar <diego.netsolutions@gmail.com>
2022-02-05 21:43:41 +0200

    sidebar completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
 
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -39,18 +38,17 @@ const closedMixin = (theme) => ({
     duration: theme.transitions.duration.leavingScreen,
   }),
   overflowX: 'hidden',
-  minWidth: `calc(${theme.spacing(7)} + 1px)`,
-  maxWidth: `calc(${theme.spacing(7)} + 1px)`
+  width: `calc(${theme.spacing(7)} + 1px)`
 });
 
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
+
+const ListSectionButton = styled(ListItem, { shouldForwardProp: (prop) => prop !== 'active' })(({ active }) => ({
+cursor:'pointer',
+backgroundColor: active ? '#f5f5f5' : '#fff',
+borderLeft: active ? '4px solid black' : 'none',
 }));
+
+
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
@@ -64,7 +62,8 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
     }),
     ...(!open && {
       ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
+      '& .MuiDrawer-paper': {...closedMixin(theme),
+      borderRight: 'none',},
     }),
   }),
 );
@@ -94,33 +93,33 @@ export default function App() {
           </div>
           <div style={{ flex: 1 }}>
             <List>
-              <ListItem button>
+              <ListSectionButton>
                 <ListItemIcon>
-                  <ListIcon color='secondary' />
+                  <ListIcon className='blackColor' />
                 </ListItemIcon>
                 <ListItemText primary={'List of Services'} />
-              </ListItem>
+              </ListSectionButton>
 
-              <ListItem button>
+              <ListSectionButton active={true}>
                 <ListItemIcon>
-                  <AssignmentIcon />
+                  <AssignmentIcon className='blackColor' />
                 </ListItemIcon>
                 <ListItemText primary={'Assignments'} />
-              </ListItem>
+              </ListSectionButton>
 
-              <ListItem button>
+              <ListSectionButton>
                 <ListItemIcon>
-                  <AssessmentIcon />
+                  <AssessmentIcon className='blackColor' />
                 </ListItemIcon>
                 <ListItemText primary={'Reports'} />
-              </ListItem>
+              </ListSectionButton>
 
-              <ListItem button>
+              <ListSectionButton>
                 <ListItemIcon>
-                  <LightModeIcon />
+                  <LightModeIcon className='blackColor' />
                 </ListItemIcon>
                 <ListItemText primary={'Insights'} />
-              </ListItem>
+              </ListSectionButton>
             </List>
           </div>
 
@@ -133,42 +132,16 @@ export default function App() {
           }}>
             <ListItemIcon >
 
-              <AccountCircleIcon />
+              <AccountCircleIcon className='blackColor' />
 
             </ListItemIcon>
             <ListItemText primary={'Account'} />
           </ListItem>
 
         </Drawer>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <DrawerHeader />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-            enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-            imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-            Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-            Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-            nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-            leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-            feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-            sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-            eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-            neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-            tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-            sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-            tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-            et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-            tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-            eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-            posuere sollicitudin aliquam ultrices sagittis orci a.
-          </Typography>
+        <Box component="main" className='mainContent'>
+
+       
         </Box>
       </Box>
     </div>
