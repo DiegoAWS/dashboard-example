c1ca588de1b7bb09bcdf4c84207421368704fe3d
Diego Escobar <diego.netsolutions@gmail.com>
2022-02-06 00:02:34 +0200

    finished upper section  card

--- src/components/SelectionUpperCard/SelectionUpperCard.jsx
+++ src/components/SelectionUpperCard/SelectionUpperCard.jsx
@@ -1,7 +1,8 @@
 
-import { ButtonGroup } from '@mui/material';
+import { ButtonGroup, Menu, MenuItem, useMediaQuery, useTheme } from '@mui/material';
 import React, { useState } from 'react';
 import WhiteCard from '../WhiteCard/WhiteCard';
+import Divider from '@mui/material/Divider';
 import DownloadIcon from '@mui/icons-material/Download';
 
 import './SelectionUpperCard.scss'
@@ -16,28 +17,87 @@ const BUTTONS_SELECTOR = [
 
 const SelectionUpperCard = () => {
     const [activeButton, setActiveButton] = useState(1);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
 
+    const theme = useTheme();
+    const upDesktop = useMediaQuery(`(min-width:${theme.breakpoints.values.lg}px)`);
+
+
+    const openMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const itemClick = (index) => {
+        setActiveButton(index)
+        setAnchorEl(null);
+    };
+
+    const closeMenu = () => {
+        setAnchorEl(null);
+    };
+
+const donwloadButtonClick = () => {
+    console.log('download')
+}
     return <WhiteCard className='SelectionUpperCardContainer'>
         <div className='titleContainer'>
             <div className='titleSection'>State</div>
-            
-        </div>
-        <div className='buttonsContainer'>
-            <ButtonGroup variant="outlined">
-                {BUTTONS_SELECTOR.map((button, index) =>
-                    <ToggleButton
-                        key={index}
-                        active={activeButton === index}
-                        onClick={() => setActiveButton(index)}
 
-                    >
-                        {button}
+        </div>
+
+        <div className='buttonsContainer'>
+            {upDesktop ?
+                <>
+                    <ButtonGroup variant="outlined">
+                        {BUTTONS_SELECTOR.map((button, index) =>
+                            <ToggleButton
+                                key={index}
+                                active={activeButton === index}
+                                onClick={() => setActiveButton(index)}
+
+                            >
+                                {button}
+                            </ToggleButton>
+                        )}
+                    </ButtonGroup>
+                    <ToggleButton active className='downloadButton' onClick={donwloadButtonClick}>
+                        <DownloadIcon />
                     </ToggleButton>
-                )}
-            </ButtonGroup>
-            <ToggleButton active className='downloadButton'>
-                <DownloadIcon />
-            </ToggleButton>
+                </>
+                : <>
+                    <ToggleButton
+                        active
+                        onClick={openMenu}
+                    >
+                        {BUTTONS_SELECTOR[activeButton]}
+                    </ToggleButton>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={closeMenu}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                    >
+                        {BUTTONS_SELECTOR.map((button, index) =>
+                            <MenuItem key={index} onClick={() => itemClick(index)}>{button}</MenuItem>
+                        )}
+
+                        <Divider />
+                        <MenuItem onClick={donwloadButtonClick}>
+                            <DownloadIcon color='info'/>
+                            Download
+                        </MenuItem>
+                    </Menu>
+                </>
+            }
+
         </div>
     </WhiteCard>
 
